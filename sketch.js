abb0388a7c1e64e22a58707ffc3ff8d0811ce5fe
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-10-17 20:36:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+
+let s0 = "Harry, Fleur, Victor, Cedrik";
+let words;
+
+let chosenIndex = -10;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0,0,0)
+  words = s0.split(", ")
+
 }
 
+
+
 function draw() {
+stroke("lightblue")
+noFill()
+//insert goblet
+ellipse(width/2, height/2, 200, 200)
+
+
+//names filling the screen randomly
+if (chosenIndex > -1) {
+  text(words[chosenIndex], random(width) , random(height) );
+}
+
+}
+function mouseClicked (){
+  chosenIndex = floor(random(0, words.length))
 }
